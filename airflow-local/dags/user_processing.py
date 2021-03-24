603049d5f078a8acd3f1f70c5038410ce0991bc9
edoquendo1314 <edoquendo1314@gmail.com>
2021-03-24 16:37:46 -0400

    Added a method to process the data and store it

--- airflow-local/dags/user_processing.py
+++ airflow-local/dags/user_processing.py
@@ -2,14 +2,32 @@ from airflow.models import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
-
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
 from datetime import datetime
+from pandas import json_normalize
 import json
 
 default_args = {
     'start_date': datetime(2021,3,22)
 }
+
+def _processing_user(ti):
+    users = ti.xcom_pull(task_ids=['extracting_user'])
+    if not len(users) or 'results' not in users[0]:
+        raise ValueError('User is empty')
+    user = users[0]['results'][0]
+    processed_user = json_normalize({
+        'firstname': user['name']['first'],
+        'lastname': user['name']['last'],
+        'country': user['location']['country'],
+        'username': user['login']['username'],
+        'password': user['login']['password'],
+        'email': user['email'],   
+    })
+    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
+
 
 with DAG(
     dag_id = 'postgres_operator_dag',
@@ -47,3 +65,14 @@ with DAG(
             log_response=True
         )
 
+        processing_user = PythonOperator(
+            task_id='processing_user',
+            python_callable=_processing_user
+        )
+
+        storing_user = BashOperator(
+            task_id='storing_user',
+            bash_command='echo -e ".separator","\n.import /tmp/processed_user.cvs users" | postgres /var/lib/postgresql/data'
+        )
+
+        creating_table >> is_api_available >> extracting_user >> processing_user >> storing_user
